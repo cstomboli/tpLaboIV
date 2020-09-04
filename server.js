5e3366e3e1bbe592f975749fe7675aae23438732
Carolina Stomboli <carostomboli@hotmail.com>
2020-09-03 21:36:17 -0300

    primer commit

---
+++ server.js
@@ -0,0 +1,7 @@
+constexpress = require('express');
+constapp = express();
+constpath = require('path');app.use(express.static(__dirname + '/angularapp'));
+app.listen(process.env.PORT || 8080);
+app.get('/*', function(req, res) 
+{res.sendFile(path.join(__dirname + '/angularapp/index.html'));});
+console.log('Build successful!!');
